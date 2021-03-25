de4719a31f30bfd9f14f893687781fe175fed5f5
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-25 12:34:06 +0530

    Refactor :check equality of two lines

--- src/com/lineparisonoops/LineComparisonOops.java
+++ src/com/lineparisonoops/LineComparisonOops.java
@@ -19,8 +19,37 @@ public class LineComparisonOops {
 		System.out.println("Enter y2 point :");
 		y2 = sc.nextInt();
 		length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-		System.out.println("Length of Line is: " + "(" + x1 + "," + y1 + ")," 
+		System.out.println("length of Line is: " + "(" + x1 + "," + y1 + ")," 
 		                        + "(" + x2 + "," + y2 + ")" + " " + length);
+		
+		int p1,p2,q2,q1;
+		double length1;
+		
+		System.out.println("Enter x1 point");
+		x1 = sc.nextInt();
+		System.out.println("Enter y1 point");
+		y1 = sc.nextInt();
+		System.out.println("Enter x2 point");
+		x2 = sc.nextInt();
+		System.out.println("Enter y2 point");
+		y2 = sc.nextInt();
+		System.out.println("Enter p1 point");
+		p1 = sc.nextInt();
+		System.out.println("Enter q1 point");
+		q1 = sc.nextInt();
+		System.out.println("Enter p2 point");
+		p2 = sc.nextInt();
+		System.out.println("Enter q2 point");
+		q2 = sc.nextInt();
+		length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		length1 = Math.sqrt((p2 - p1) ^ 2 + (q2 - q1) ^ 2);
+		//System.out.println("Length of Line is: " + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")" 	+ Length);
+		if(length == length1) {
+			System.out.println("Length is equal to length1");
+		}
+		else {
+			System.out.println("Length is not equal to length1");
+		}
 		
 
 	}
