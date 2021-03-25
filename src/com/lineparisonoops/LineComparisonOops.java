e63d3954824c42928d956c826de323183e4f57e9
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-25 12:22:27 +0530

    check equality of two lines

--- src/com/lineparisonoops/LineComparisonOops.java
+++ src/com/lineparisonoops/LineComparisonOops.java
@@ -1,9 +1,27 @@
 package com.lineparisonoops;
 
+import java.util.Scanner;
+
 // Creating a method 
 public class LineComparisonOops {
 	public void lineComparison() {
 		System.out.println("Welcome to Line Comparsion Problem");
+
+		int x1, x2, y1, y2;
+		double length;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x1 point :");
+		x1 = sc.nextInt();
+		System.out.println("Enter y1 point :");
+		y1 = sc.nextInt();
+		System.out.println("Enter x2 point :");
+		x2 = sc.nextInt();
+		System.out.println("Enter y2 point :");
+		y2 = sc.nextInt();
+		length = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Length of Line is: " + "(" + x1 + "," + y1 + ")," 
+		                        + "(" + x2 + "," + y2 + ")" + " " + length);
+		
 
 	}
 
