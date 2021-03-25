437e02cd3f46e73b531595043c207a516457817f
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-25 12:06:37 +0530

    Welcome message

---
+++ src/com/lineparisonoops/LineComparisonOops.java
@@ -0,0 +1,17 @@
+package com.lineparisonoops;
+
+// Creating a method 
+public class LineComparisonOops {
+	public void lineComparison() {
+		System.out.println("Welcome to Line Comparsion Problem");
+
+	}
+
+	public static void main(String[] args) {
+
+		LineComparisonOops ls = new LineComparisonOops();
+		ls.lineComparison();
+
+	}
+
+}
